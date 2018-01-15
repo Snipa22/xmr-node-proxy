5f5ea5db58c739f489bd0f34a85e55e032c22a09
Venthos <venthos@venthos.net>
2018-01-15 12:09:39 -0600

    Adjust share percentage and revert to xmr-node-proxy agent
    - Realization kicked in that by adjusting the agent name, Snipa22
      donations no longer worked since his pool didn't understand the agent.
      So, I've suffixed "-intense" onto it to help identify in logs but
      keep compatibility.
    - It helps to read and understand a config first... sigh. I've adjusted
      the shares in the config example so they add up to 100 (99 pool / 1 dev)

--- proxy.js
+++ proxy.js
@@ -260,7 +260,7 @@ function Pool(poolData){
         this.sendData('login', {
             login: this.username,
             pass: this.password,
-            agent: 'intense-node-proxy/0.0.1'
+            agent: 'xmr-node-proxy/0.0.1-intense'
         });
         this.active = true;
         for (let worker in cluster.workers){
