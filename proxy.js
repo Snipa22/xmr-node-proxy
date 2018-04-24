2f75f70f550b5a014bb8bd1cb499686852f74094
Learner <linhdqtg@gmail.com>
2018-04-24 15:06:36 +0700

    Fix undefined identifier and add 15s refresh (#11)
    * Fix undefined identifier and add 15s refresh
    
    * Remove useless check.
    
    * Check if miner is null ! How can it be null ?
    
    I'm thinking about running http only on Master

--- proxy.js
+++ proxy.js
@@ -1024,6 +1024,7 @@ function activateHTTP() {
 				for (let minerID in activeWorkers[workerID]){
                 			if (!activeWorkers[workerID].hasOwnProperty(minerID)) continue;
 					let miner = activeWorkers[workerID][minerID];
+					if (!miner) continue;	
 					let name = (miner.identifier && miner.identifier != "x") ? miner.identifier + " (" + miner.ip + ")" : miner.ip;
 					++ totalWorkers;
 					totalHashrate += miner.avgSpeed;
@@ -1046,6 +1047,7 @@ function activateHTTP() {
 <html lang="en"><head>
 	<title>XNP Hashrate Monitor</title>
 	<meta charset="utf-8">
+	<meta http-equiv="refresh" content="15">
 	<style>
 	  html, body {
 	    font-family: 'Saira Semi Condensed', sans-serif;
