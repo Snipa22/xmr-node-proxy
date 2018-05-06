19e072945b393258ed7f44bc69d18f8c0b067a6d
MoneroOcean <support@moneroocean.stream>
2018-05-06 10:14:58 +0200

    Updated update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-git checkout .
-git pull
-npm install
-echo "Proxy updated!  Please go ahead and restart with the correct pm2 command"
+#!/bin/bash
+git checkout . &&\
+git pull &&\
+npm install &&\
+echo "Proxy updated OK! Please go ahead and restart with the correct pm2 command"
 echo "This is usually pm2 restart proxy, however, you can use pm2 list to check for your exact proxy command"
