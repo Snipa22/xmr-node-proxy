2467b13be3f25fb904c23ac93f7900bb834db181
Learner <linhdqtg@gmail.com>
2018-06-19 02:41:05 +0700

    Add http message when access is denied
    It gives better info.

--- proxy.js
+++ proxy.js
@@ -1094,10 +1094,12 @@ function activateHTTP() {
 		if (global.config.httpUser && global.config.httpPass) {
 			var auth = req.headers['authorization'];  // auth is in base64(username:password)  so we need to decode the base64
 			if (!auth) {
-				res.writeHead(401);
-				res.end();
+				res.statusCode = 401;
+				res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+				res.end('<html><body>Unauthorized XNP access.</body></html>');
 				return;
 			}
+			console.log("Authorization Header is: ", auth);
 			var tmp = auth.split(' ');
 	                var buf = new Buffer(tmp[1], 'base64');
         	        var plain_auth = buf.toString();
@@ -1105,8 +1107,9 @@ function activateHTTP() {
 			var username = creds[0];
 			var password = creds[1];
 			if (username !== global.config.httpUser || password !== global.config.httpPass) {
-				res.writeHead(401);
-				res.end();
+				res.statusCode = 401;
+				res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+				res.end('<html><body>Wrong login.</body></html>');
 				return;
 			}
 		}
