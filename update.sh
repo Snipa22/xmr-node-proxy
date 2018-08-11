85c84fed48e18f1cfc62ba152933350cfe24108b
MoneroOcean <support@moneroocean.stream>
2018-08-11 02:59:05 +0200

    Added auto pull

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git checkout . &&\
-git pull &&\
+git pull --no-edit &&\
 npm install &&\
 echo "Proxy updated OK! Please go ahead and restart with the correct pm2 command"
 echo "This is usually pm2 restart proxy, however, you can use pm2 list to check for your exact proxy command"
