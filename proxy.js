eab5a7c60e1b49fabcef8f2d0fe65c4e8e3b747d
MoneroOcean <support@moneroocean.stream>
2019-03-18 08:51:23 -0700

    Moved to updated LOKI utils

--- proxy.js
+++ proxy.js
@@ -10,7 +10,7 @@ const uuidV4 = require('uuid/v4');
 const support = require('./lib/support.js')();
 global.config = require('./config.json');
 
-const PROXY_VERSION = "0.9.0";
+const PROXY_VERSION = "0.9.1";
 const DEFAULT_ALGO      = [ "cn/r" ];
 const DEFAULT_ALGO_PERF = { "cn": 1, "cn/half": 1.9, "cn/rwz": 1.3, "cn/zls": 1.3, "cn/double": 0.5 };
 
